776c8510d6c0da433bcc3895ec1b20ed4e986739
Nataliya Alexandrova <naleksandrovav@gmail.com>
2021-02-12 21:55:47 +0300

    Рефакторинг task2

--- task2/src/task2.java
+++ task2/src/task2.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class task2 {
+
     private static class Point {
         public final float x;
         public final float y;
@@ -16,76 +17,51 @@ public class task2 {
     }
 
     public static void main(String[] arg) throws IOException {
-        Path path1 = Path.of("c:\\Users\\Lecsik\\Documents\\filesforexample\\poligon.txt");
-        Path path2 = Path.of("c:\\Users\\Lecsik\\Documents\\filesforexample\\readme2.txt");
+        Path polygonPath = Path.of(arg[0]);
+        Path pointPath = Path.of(arg[1]);
 
-        if (Files.exists(path1) && Files.exists(path2)) {
+        List<String> polygonString = Files.readAllLines(polygonPath);
+        List<String> pointsStrings = Files.readAllLines(pointPath);
+        List<Point> polygon = new ArrayList<>();
+        List<Point> points = new ArrayList<>();
 
-            List<String> poligonString = Files.readAllLines(path1);
-            List<String> pointString = Files.readAllLines(path2);
-            List<Point> poligonFinal = new ArrayList<>();
-            List<Point> pointFinal = new ArrayList<>();
+        for (String s : polygonString) {
+            String[] subStr = s.split(" ");
+            polygon.add(new Point(Float.parseFloat(subStr[0]), Float.parseFloat(subStr[1])));
+        }
+        for (String s : pointsStrings) {
+            String[] subStr = s.split(" ");
+            points.add(new Point(Float.parseFloat(subStr[0]), Float.parseFloat(subStr[1])));
+        }
 
-            for (String s : poligonString) {
-                String[] subStr;
-                String delimeter = " ";
-                subStr = s.split(delimeter);
-                poligonFinal.add(new Point(Float.parseFloat(subStr[0]), Float.parseFloat(subStr[1])));
-            }
-            for (String s : pointString) {
-                String[] subStr;
-                String delimeter = " ";
-                subStr = s.split(delimeter);
-
-                pointFinal.add(new Point(Float.parseFloat(subStr[0]), Float.parseFloat(subStr[1])));
-
-            }
-
-            for (Point point : pointFinal) {
-                System.out.println(pointPlace(point, poligonFinal));
-            }
+        for (Point point : points) {
+            System.out.println(pointPlace(point, polygon));
         }
     }
 
 
     public static int pointPlace(Point point, List<Point> polygon) {
-        int j;
-        int result;
-        boolean resIns = false;
-        boolean resOnLine = false;
-        boolean resOnPoint = false;
-
         for (Point value : polygon) {
-            if (point.x == value.x && point.y == value.y) resOnPoint = !resOnPoint;
-
+            if (point.x == value.x && point.y == value.y) return 0;
         }
-        if (resOnPoint) result = 0;
 
-        else {
-            j = polygon.size() - 1;
-            for (int i = 0; i < polygon.size(); i++) {
-                if ((point.y - polygon.get(i).y) * (polygon.get(j).x - polygon.get(i).x) == (point.x - polygon.get(i).x) * (polygon.get(j).y - polygon.get(i).y))
-                    resOnLine = !resOnLine;
-                j = i;
-            }
-            if (resOnLine) result = 1;
-            else {
-                j = polygon.size() - 1;
-                for (int i = 0; i < polygon.size(); i++) {
-
-                    if ((((polygon.get(i).y < point.y) && (point.y < polygon.get(j).y)) || ((polygon.get(j).y <= point.y) && (point.y < polygon.get(i).y))) &&
-                            (point.x > (polygon.get(j).x - polygon.get(i).x) * (point.y - polygon.get(i).y) / (polygon.get(j).y - polygon.get(i).y) + polygon.get(i).x))
-                        resIns = !resIns;
-                    j = i;
-                }
-
-                if (resIns) result = 2;
-                else result = 3;
-            }
-
+        int j = polygon.size() - 1;
+        for (int i = 0; i < polygon.size(); i++) {
+            if ((point.y - polygon.get(i).y) * (polygon.get(j).x - polygon.get(i).x) == (point.x - polygon.get(i).x) * (polygon.get(j).y - polygon.get(i).y))
+                return 1;
+            j = i;
         }
-        return result;
+
+        j = polygon.size() - 1;
+        boolean resIns = false;
+        for (int i = 0; i < polygon.size(); i++) {
+            if ((((polygon.get(i).y < point.y) && (point.y < polygon.get(j).y)) || ((polygon.get(j).y <= point.y) && (point.y < polygon.get(i).y))) &&
+                    (point.x > (polygon.get(j).x - polygon.get(i).x) * (point.y - polygon.get(i).y) / (polygon.get(j).y - polygon.get(i).y) + polygon.get(i).x))
+                resIns = !resIns;
+            j = i;
+        }
+
+        if (resIns) return 2;
+        else return 3;
     }
 }
-
-

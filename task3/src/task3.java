600aca71027774897812bb3f90c4093075c3ec3a
Nataliya Alexandrova <naleksandrovav@gmail.com>
2021-02-12 22:05:38 +0300

    Рефакторинг task3

--- task3/src/task3.java
+++ task3/src/task3.java
@@ -5,46 +5,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class task3 {
-    public static void main(String[] arg) throws IOException {
-        Path path1 = Path.of("c:\\Users\\Lecsik\\Documents\\filesforexample\\Cash1.txt");
-        Path path2 = Path.of("c:\\Users\\Lecsik\\Documents\\filesforexample\\Cash2.txt");
-        Path path3 = Path.of("c:\\Users\\Lecsik\\Documents\\filesforexample\\Cash3.txt");
-        Path path4 = Path.of("c:\\Users\\Lecsik\\Documents\\filesforexample\\Cash4.txt");
-        Path path5 = Path.of("c:\\Users\\Lecsik\\Documents\\filesforexample\\Cash5.txt");
-        if (Files.exists(path1) && Files.exists(path2) && Files.exists(path3) && Files.exists(path4) && Files.exists(path5)) {
 
-            List<String> list1 = Files.readAllLines(path1);
-            List<String> list2 = Files.readAllLines(path2);
-            List<String> list3 = Files.readAllLines(path3);
-            List<String> list4 = Files.readAllLines(path4);
-            List<String> list5 = Files.readAllLines(path5);
-            ArrayList<Float> list1final = new ArrayList<>();
-            ArrayList<Float> list2final = new ArrayList<>();
-            ArrayList<Float> list3final = new ArrayList<>();
-            ArrayList<Float> list4final = new ArrayList<>();
-            ArrayList<Float> list5final = new ArrayList<>();
-            ArrayList<Float> sumInterv = new ArrayList<>();
-            int maxInd = -1;
-            float max = 0;
-            for (int i = 0; i < list1.size(); i++) {
-                list1final.add(Float.parseFloat(list1.get(i)));
-                list2final.add(Float.parseFloat(list2.get(i)));
-                list3final.add(Float.parseFloat(list3.get(i)));
-                list4final.add(Float.parseFloat(list4.get(i)));
-                list5final.add(Float.parseFloat(list5.get(i)));
-            }
-            for (int i = 0; i < list1.size(); i++) {
-                sumInterv.add(i, (list1final.get(i) + list2final.get(i) + list3final.get(i) + list4final.get(i) + list5final.get(i)));
-            }
-            for (int i = 0; i < sumInterv.size(); i++) {
-                if (sumInterv.get(i) > max) {
-                    max = sumInterv.get(i);
-                    maxInd = i;
-                }
-            }
-            System.out.println(maxInd + 1);
+    public static void main(String[] arg) throws IOException {
+        Path path1 = Path.of(arg[0]);
+        Path path2 = Path.of(arg[1]);
+        Path path3 = Path.of(arg[2]);
+        Path path4 = Path.of(arg[3]);
+        Path path5 = Path.of(arg[4]);
+
+        List<String> strings1 = Files.readAllLines(path1);
+        List<String> strings2 = Files.readAllLines(path2);
+        List<String> strings3 = Files.readAllLines(path3);
+        List<String> strings4 = Files.readAllLines(path4);
+        List<String> strings5 = Files.readAllLines(path5);
+
+        ArrayList<Float> sumInterv = new ArrayList<>();
+        for (int i = 0; i < strings1.size(); i++) {
+            sumInterv.add(Float.parseFloat(strings1.get(i)) +
+                    Float.parseFloat(strings2.get(i)) +
+                    Float.parseFloat(strings3.get(i)) +
+                    Float.parseFloat(strings4.get(i)) +
+                    Float.parseFloat(strings5.get(i)));
         }
 
+        int maxInd = -1;
+        float max = 0;
+        for (int i = 0; i < sumInterv.size(); i++) {
+            if (sumInterv.get(i) > max) {
+                max = sumInterv.get(i);
+                maxInd = i;
+            }
+        }
+        System.out.println(maxInd + 1);
     }
-
 }
